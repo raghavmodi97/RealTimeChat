d73639e5a29d8dacfcb7e33d0690c40b60221c24
raghavmodi97 <100182915+raghavmodi97@users.noreply.github.com>
2025-11-13 17:11:01 +0000

    WebSocketIntegration

---
+++ realTimeChatServer/realtimechat/src/main/java/com/realtimechat/realtimechat/RealtimechatApplication.java
@@ -0,0 +1,13 @@
+package com.realtimechat.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimechatApplication.class, args);
+	}
+
+}
