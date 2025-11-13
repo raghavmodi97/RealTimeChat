dab69ee144644fe88be9b1a0ff679679ce175e46
raghavmodi97 <100182915+raghavmodi97@users.noreply.github.com>
2025-11-13 18:49:50 +0000

    MessageEntity

---
+++ realTimeChatServer/realtimechat/src/main/java/com/realtimechat/realtimechat/model/Message.java
@@ -0,0 +1,24 @@
+package com.realtimechat.realtimechat.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.Instant;
+
+@Entity
+@Table(name = "messages")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long conversationId;
+    private Long senderId;
+    private String content;
+    private Instant createdAt = Instant.now();
+}
